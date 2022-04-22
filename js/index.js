346fdb50f3dc8f09f403c13adf03bbf8d029c949
Julieth Sierra <juliethaleja2000@gmail.com>
2022-04-21 22:01:49 -0500

    agregar json variables

--- js/index.js
+++ js/index.js
@@ -183,6 +183,104 @@ function tipo_pago() {
     }
   }
 } */
+const producto=[
+  {
+    id:1,
+    modelo:'Tenis OwnTheGame',
+    marca:'Adidas',
+    precio:339.990,
+    talla:['UN','3'],
+    img:'img/ADIDAS1.png'
+  },
+  {
+    id:2,
+    modelo:'Air Jordan 7 Retro SE',
+    marca:'Jordan',
+    precio:784.000,
+    talla:['16','4'],
+    img:'img/JORDAN1.png'
+  },
+  {
+    id:3,
+    modelo:'Kyrie Infinity',
+    marca:'Nike',
+    precio:484.900,
+    talla:['10','12'],
+    img:'img/NIKE 1.png',
+  },
+  {
+    id:4,
+    modelo:'Tenis de Basketball UA GS Lockdown 5',
+    marca:'Under Amuor',
+    precio:339.990,
+    talla:['14','7'],
+    img:'img/UNAM1.png'
+  },
+  {
+    id:5,
+    modelo:'Air Jordan OG',
+    marca:'Jordan',
+    precio:522.900,
+    talla:['UN','8'],
+    img:'img/JORDAN2.png'
+  },
+  {
+    id:6,
+    modelo:'Tenis Postmove Mid',
+    marca:'Adidas',
+    precio:369.990,
+    talla:['3','4'],
+    img:'img/ADIDAS2.png',
+  },
+  {
+    id:7,
+    modelo:'Nike Air Zoom G.T. Jump',
+    marca:'Nike',
+    precio:671.990,
+    talla:['12','16'],
+    img:'img/NIKE2.png',
+  },
+  {
+    id:8,
+    modelo:'Tenis Cross Em Up 5 K Anchos',
+    marca:'Adidas',
+    precio:279.990,
+    talla:['7','4'],
+    img:'img/ADIDAS3.png',
+  },
+  {
+    id:9,
+    modelo:'UA Jet 21 Unisex',
+    marca:'Under Amour',
+    precio:359.990,
+    talla:['16','10'],
+    img:'iimg/UNAM2.png',
+  },
+  {
+    id:10,
+    modelo:'Air Jordan XXXVI',
+    marca:'Jordan',
+    precio:689.990,
+    talla:['12','14'],
+    img:'img/JORDAN3.png',
+  },
+  {
+    id:11,
+    modelo:'UA HOVR Havoc 3',
+    marca:'Under Amour',
+    precio:245.990,
+    talla:['14','UN'],
+    img:'img/UNAM3.png',
+  },
+  {
+    id:12,
+    modelo:'Zion 1',
+    marca:'Nike',
+    precio:313.990,
+    talla:['14','7'],
+    img:'img/NIKE3.png',
+  },
+]
 let base=document.getElementById("base");
 let class_nike = document.getElementsByClassName("nike");
 let class_adidas = document.getElementsByClassName("adidas");
