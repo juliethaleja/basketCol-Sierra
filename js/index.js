ca2bbf50d985228f0074590dab04a7f4d0c48977
Julieth Sierra <juliethaleja2000@gmail.com>
2022-04-24 13:52:15 -0500

    Variables Json

--- js/index.js
+++ js/index.js
@@ -183,172 +183,244 @@ function tipo_pago() {
     }
   }
 } */
-const producto=[
+const producto = [
   {
-    id:1,
-    modelo:'Tenis OwnTheGame',
-    marca:'Adidas',
-    precio:339.990,
-    talla:['UN','3'],
-    img:'img/ADIDAS1.png'
+    id: 1,
+    modelo: 'Tenis OwnTheGame',
+    marca: 'Adidas',
+    precio: '339.990',
+    talla: ['UN', '3'],
+    img: 'img/ADIDAS1.png'
   },
   {
-    id:2,
-    modelo:'Air Jordan 7 Retro SE',
-    marca:'Jordan',
-    precio:784.000,
-    talla:['16','4'],
-    img:'img/JORDAN1.png'
+    id: 2,
+    modelo: 'Air Jordan 7 Retro SE',
+    marca: 'Jordan',
+    precio: '784.000',
+    talla: ['16', '4'],
+    img: 'img/JORDAN1.png'
   },
   {
-    id:3,
-    modelo:'Kyrie Infinity',
-    marca:'Nike',
-    precio:484.900,
-    talla:['10','12'],
-    img:'img/NIKE 1.png',
+    id: 3,
+    modelo: 'Kyrie Infinity',
+    marca: 'Nike',
+    precio: '484.900',
+    talla: ['10', '12'],
+    img: 'img/NIKE 1.png',
   },
   {
-    id:4,
-    modelo:'Tenis de Basketball UA GS Lockdown 5',
-    marca:'Under Amuor',
-    precio:339.990,
-    talla:['14','7'],
-    img:'img/UNAM1.png'
+    id: 4,
+    modelo: 'Tenis de Basketball UA GS Lockdown 5',
+    marca: 'Under Amuor',
+    precio: '339.990',
+    talla: ['14', '7'],
+    img: 'img/UNAM1.png'
   },
   {
-    id:5,
-    modelo:'Air Jordan OG',
-    marca:'Jordan',
-    precio:522.900,
-    talla:['UN','8'],
-    img:'img/JORDAN2.png'
+    id: 5,
+    modelo: 'Air Jordan OG',
+    marca: 'Jordan',
+    precio: '522.900',
+    talla: ['UN', '8'],
+    img: 'img/JORDAN2.png'
   },
   {
-    id:6,
-    modelo:'Tenis Postmove Mid',
-    marca:'Adidas',
-    precio:369.990,
-    talla:['3','4'],
-    img:'img/ADIDAS2.png',
+    id: 6,
+    modelo: 'Tenis Postmove Mid',
+    marca: 'Adidas',
+    precio: '369.990',
+    talla: ['3', '4'],
+    img: 'img/ADIDAS2.png',
   },
   {
-    id:7,
-    modelo:'Nike Air Zoom G.T. Jump',
-    marca:'Nike',
-    precio:671.990,
-    talla:['12','16'],
-    img:'img/NIKE2.png',
+    id: 7,
+    modelo: 'Nike Air Zoom G.T. Jump',
+    marca: 'Nike',
+    precio: '671.990',
+    talla: ['12', '16'],
+    img: 'img/NIKE2.png',
   },
   {
-    id:8,
-    modelo:'Tenis Cross Em Up 5 K Anchos',
-    marca:'Adidas',
-    precio:279.990,
-    talla:['7','4'],
-    img:'img/ADIDAS3.png',
+    id: 8,
+    modelo: 'Tenis Cross Em Up 5 K Anchos',
+    marca: 'Adidas',
+    precio: '279.990',
+    talla: ['7', '4'],
+    img: 'img/ADIDAS3.png',
   },
   {
-    id:9,
-    modelo:'UA Jet 21 Unisex',
-    marca:'Under Amour',
-    precio:359.990,
-    talla:['16','10'],
-    img:'iimg/UNAM2.png',
+    id: 9,
+    modelo: 'UA Jet 21 Unisex',
+    marca: 'Under Amour',
+    precio: '359.990',
+    talla: ['16', '10'],
+    img: 'img/UNAM2.png',
   },
   {
-    id:10,
-    modelo:'Air Jordan XXXVI',
-    marca:'Jordan',
-    precio:689.990,
-    talla:['12','14'],
-    img:'img/JORDAN3.png',
+    id: 10,
+    modelo: 'Air Jordan XXXVI',
+    marca: 'Jordan',
+    precio: '689.990',
+    talla: ['12', '14'],
+    img: 'img/JORDAN3.png',
   },
   {
-    id:11,
-    modelo:'UA HOVR Havoc 3',
-    marca:'Under Amour',
-    precio:245.990,
-    talla:['14','UN'],
-    img:'img/UNAM3.png',
+    id: 11,
+    modelo: 'UA HOVR Havoc 3',
+    marca: 'Under Amour',
+    precio: '245.990',
+    talla: ['14', 'UN'],
+    img: 'img/UNAM3.png',
   },
   {
-    id:12,
-    modelo:'Zion 1',
-    marca:'Nike',
-    precio:313.990,
-    talla:['14','7'],
-    img:'img/NIKE3.png',
+    id: 12,
+    modelo: 'Zion 1',
+    marca: 'Nike',
+    precio: '313.990',
+    talla: ['14', '7'],
+    img: 'img/NIKE3.png',
   },
-]
-let base=document.getElementById("base");
+];
+const pesos = '$';
+const productos = document.querySelector('#productos');
+
+function EstructuraProductos() {
+  producto.forEach((info) => {
+    //base
+    const estructura = document.createElement('div');
+    estructura.classList.add('col-sm-4');
+    // Estructura
+    const card = document.createElement('div');
+    card.classList.add('card');
+    // Body
+    const CardBody = document.createElement('div');
+    CardBody.classList.add('card-content');
+    // Titulo
+    const CardTitle = document.createElement('h5');
+    CardTitle.classList.add('title', 'is-7');
+    CardTitle.textContent = info.modelo;
+    // Imagen
+    const CardImagen = document.createElement('div');
+    const CardFigure = document.createElement('figure');
+    const Img = document.createElement('img');
+    CardImagen.classList.add('card-image');
+    CardFigure.classList.add('image', 'is-4by3');
+    Img.setAttribute('src', info.img);
+    CardFigure.insertAdjacentElement("afterbegin", Img);
+    CardImagen.insertAdjacentElement("afterbegin", CardFigure);
+    // Precio
+    const text_precio = document.createElement('p');
+    text_precio.classList.add('subtitle', 'is-8');
+    text_precio.textContent = `${pesos}${info.precio}`;
+
+    const text_marca = document.createElement('p');
+    text_marca.classList.add('is-8');
+    text_marca.textContent = `Marca ${info.marca}`;
+
+    const text_talla = document.createElement('div');
+    const select_talla = document.createElement('select');
+    let tallas = info.talla;
+    let select = `<select>
+                  <option>Talla</option>`
+    tallas.forEach(value => {
+      select += `<option> ${value}</option>`;
+    });
+    select += `</select><br/>`;
+
+    select_talla.innerHTML = select;
+
+    text_talla.classList.add('select', 'is-primary');
+    text_talla.insertAdjacentElement("beforeend", select_talla);
+
+    // Boton 
+    /*     const miNodoBoton = document.createElement('button');
+        miNodoBoton.classList.add('btn', 'btn-primary');
+        miNodoBoton.textContent = '+';
+        miNodoBoton.setAttribute('marcador', info.id);
+        miNodoBoton.addEventListener('click', anyadirProductoAlCarrito); */
+    // Insertamos
+
+    estructura.appendChild(card);
+    CardBody.appendChild(CardTitle);
+    CardBody.appendChild(text_precio);
+    CardBody.appendChild(text_marca);
+    CardBody.appendChild(text_talla);
+    /*  CardBody.appendChild(miNodoBoton);*/
+    card.appendChild(CardImagen);
+    card.appendChild(CardBody);
+    productos.appendChild(estructura);
+  });
+}
+EstructuraProductos();
+
+let base = document.getElementById("base");
 let class_nike = document.getElementsByClassName("nike");
 let class_adidas = document.getElementsByClassName("adidas");
 let class_unam = document.getElementsByClassName("unam");
 let class_jordan = document.getElementsByClassName("jordan");
 let jordan = document.getElementById("jordan");
 jordan.onclick = () => {
-  
+
   for (var i = 0; i < class_nike.length; i++) {
-class_nike[i].style.display='none'
- }
+    class_nike[i].style.display = 'none'
+  }
   for (var i = 0; i < class_adidas.length; i++) {
-    class_adidas[i].style.display='none'
- }
+    class_adidas[i].style.display = 'none'
+  }
   for (var i = 0; i < class_unam.length; i++) {
-    class_unam[i].style.display='none'
- }
-   for (var i = 0; i < class_jordan.length; i++) {
-    class_jordan[i].style.display='block'
-    base.appendChild(class_jordan[i]);  
+    class_unam[i].style.display = 'none'
+  }
+  for (var i = 0; i < class_jordan.length; i++) {
+    class_jordan[i].style.display = 'block'
+    base.appendChild(class_jordan[i]);
   }
 };
-let nike=document.getElementById("nike");
+let nike = document.getElementById("nike");
 nike.onclick = () => {
   let insert;
   for (var i = 0; i < class_jordan.length; i++) {
-    class_jordan[i].style.display='none'
- }
+    class_jordan[i].style.display = 'none'
+  }
   for (var i = 0; i < class_adidas.length; i++) {
-    class_adidas[i].style.display='none'
- }
+    class_adidas[i].style.display = 'none'
+  }
   for (var i = 0; i < class_unam.length; i++) {
-    class_unam[i].style.display='none'
- }
-   for (var i = 0; i < class_nike.length; i++) {
-    class_nike[i].style.display='block'
-    insert=base.appendChild(class_nike[i]);  
+    class_unam[i].style.display = 'none'
+  }
+  for (var i = 0; i < class_nike.length; i++) {
+    class_nike[i].style.display = 'block'
+    insert = base.appendChild(class_nike[i]);
   }
 };
-let adidas=document.getElementById("adidas");
+let adidas = document.getElementById("adidas");
 adidas.onclick = () => {
   for (var i = 0; i < class_jordan.length; i++) {
-    class_jordan[i].style.display='none'
- }
+    class_jordan[i].style.display = 'none'
+  }
   for (var i = 0; i < class_nike.length; i++) {
-    class_nike[i].style.display='none'
- }
+    class_nike[i].style.display = 'none'
+  }
   for (var i = 0; i < class_unam.length; i++) {
-    class_unam[i].style.display='none'
- }
-   for (var i = 0; i < class_adidas.length; i++) {
-    class_adidas[i].style.display='block'
-    base.appendChild(class_adidas[i]);  
+    class_unam[i].style.display = 'none'
+  }
+  for (var i = 0; i < class_adidas.length; i++) {
+    class_adidas[i].style.display = 'block'
+    base.appendChild(class_adidas[i]);
   }
 };
-let umna=document.getElementById("unam");
+let umna = document.getElementById("unam");
 umna.onclick = () => {
   for (var i = 0; i < class_jordan.length; i++) {
-    class_jordan[i].style.display='none'
- }
+    class_jordan[i].style.display = 'none'
+  }
   for (var i = 0; i < class_nike.length; i++) {
-    class_nike[i].style.display='none'
- }
+    class_nike[i].style.display = 'none'
+  }
   for (var i = 0; i < class_adidas.length; i++) {
-    class_adidas[i].style.display='none'
- }
-   for (var i = 0; i < class_unam.length; i++) {
-    class_unam[i].style.display='block'
-    base.appendChild(class_unam[i]);  
+    class_adidas[i].style.display = 'none'
+  }
+  for (var i = 0; i < class_unam.length; i++) {
+    class_unam[i].style.display = 'block'
+    base.appendChild(class_unam[i]);
   }
 };
